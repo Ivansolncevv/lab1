0f39a29dde9eee9376d9ae2795ecf9ea6df75dac
unknown <admin@VLAN-25-IKK-740.rd.mirea.ru>
2017-03-03 14:17:32 +0300

    Initial commit

---
+++ src/pack/Main.java
@@ -0,0 +1,28 @@
+package pack;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        HashMap<String, Integer> m = new HashMap<>();
+        try (Scanner s = new Scanner(new File("book.txt"), "Cp1251")){
+            while(s.hasNext()) {
+                String word = s.next();
+                if (m.containsKey(word)) {
+                   int count = m.get(word);
+                   count++;
+                   m.put(word, count);
+                } else {
+                    m.put(word, 1);
+                }
+            }
+        }
+        Set<Map.Entry<String, Integer>> set = m.entrySet();
+        ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(set);
+        list.sort((e1, e2) ->- e1.getValue().compareTo(e2.getValue()));
+        for (Map.Entry<String, Integer> e: list) {
+            System.out.println(e);
+        }
+    }
+}
